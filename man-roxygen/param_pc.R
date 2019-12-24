52f26e917555d4a6c3e37ac3fd1ba6f017fa113a
Randy Lai <randy.cs.lai@gmail.com>
2019-12-23 21:52:18 -0800

    fix docs

--- man-roxygen/param_pc.R
+++ man-roxygen/param_pc.R
@@ -1,6 +1,6 @@
 #' @param x an integer or a vector, will be treated as \code{n} if integer; otherwise, will be treated as \code{v}.
 #'          Should not be specified together with \code{n} and \code{v}.
-#' @param k an integer, the number of items drawn, defaults to \code{n} if \c{ode{freq} is \code{NULL} else \code{sum(freq)}
+#' @param k an integer, the number of items drawn, defaults to \code{n} if \code{freq} is \code{NULL} else \code{sum(freq)}
 #' @param n an integer, the total number of items, its value may be implicitly deduced from \code{length(v)} or \code{length(freq)}
 #' @param v a vector to be drawn, defaults to \code{1:n}.
 #' @param freq an integer vector of item repeat frequencies
