0d08397cf9f555ad3cf9835f11caec69d451d0e8
Randy Lai <randy.cs.lai@gmail.com>
2020-01-01 00:42:43 -0800

    mpz_export writes nothing when z = 0

--- src/utils.c
+++ src/utils.c
@@ -282,7 +282,6 @@ static int raw_size(mpz_t z) {
 // from biginteger::as_raw
 // https://github.com/cran/gmp/blob/cb2935d6a7948e85e42b1402c6f5a3450547f0cf/src/biginteger.cc#L75
 SEXP mpz_to_bigz1(mpz_t z) {
-
     int size = raw_size(z);
     SEXP ans = PROTECT(Rf_allocVector(RAWSXP, size + sizeof(int)));
     unsigned char* raw = RAW(ans);
@@ -290,8 +289,12 @@ SEXP mpz_to_bigz1(mpz_t z) {
     r[0] = 1; // scalar RAWSXP
     r[1] = size / sizeof(int) - 2;
     r[2] = (int) mpz_sgn(z);
-    mpz_export(&r[3], 0, 1, sizeof(int), 0, 0, z);
-
+    if (mpz_sgn(z) == 0) {
+        // mpz_export writes nothing when z = 0
+        r[3] = 0;
+    } else {
+        mpz_export(&r[3], 0, 1, sizeof(int), 0, 0, z);
+    }
     Rf_setAttrib(ans, R_ClassSymbol, Rf_mkString("bigz"));
     UNPROTECT(1);
     return ans;
